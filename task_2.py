e793b19fe9f69634e770700fa4a9b29625112470
JYRJ <jyrjanon@gmail.com>
2025-08-17 07:17:48 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,6 @@
+import math
+
+num = int(input("Enter a number:"))
+print(f"The square root of {num} : {math.sqrt(num)}")
+print(f"The Natural Logarithm of {num} : {math.log(num)}")
+print(f"The Sine of {num} : {math.sin(num)}")
